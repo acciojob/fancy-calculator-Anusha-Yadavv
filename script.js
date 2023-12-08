9fb759792d96ab094ac8030d16304fae2225304a
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-08 17:01:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function fancy_calculator(v1,v2) {
 	v1=parseInt(v1)*2;
 	v2=parseInt(v2);
 	let result=v1+v2
-	return `The final result is:+result`;;
+	return `The final result is:+${result}`;;
 	
 }
 let value1 = prompt("Enter Start of the Range.");
