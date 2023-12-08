a70c6fb7a13a2df092c86894e683f9e3fa8cc250
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-08 16:45:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+
+
+function fancy_calculator(v1,v2) {
+	v1=v1*2;
+	let result=v1+v2;
+	console.log(`The final result is:+${result}`);
+	
+}
+
+let value1=promt();
+let value2=promt();
+alert(fancy_calculator);
+
