f7870e61d9105ac741e04259306a5501a1de27fa
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-08 17:12:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,16 +2,9 @@
 
 
 
-function fancy_calculator(v1,v2) {
-	
-	v1=parseInt(v1)*2;
-	v2=parseInt(v2);
-	let result=v1+v2
-	return `The final result is:+${result}`;;
-	
-}
-let value1 = prompt("Enter Start of the Range.");
-let value2 = prompt("Enter End Of the Range.");
-
-alert(fancy_calculator(value1,value2));
+let value1 = prompt("Enter value1.");
+let value2 = prompt("Enter value2.");
+value1=parseInt(value1)*2;
+let result=value1+parseInt(value2);
+alert("The final result is: " + result);
 
